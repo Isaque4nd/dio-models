a9642fc3b42087313c7ad54e0ba4bd1fbb9f9571
Isaque Andrade <andradeisq1.7@gmail.com>
2023-09-27 16:04:18 -0300

    adição da classe calculadora e aleterações na classe Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
 ﻿using diomodels.Models;
 
-Pessoa pessoa = new Pessoa();
 
-pessoa.Nome = "Isaque";
-pessoa.Idade = 21;
-pessoa.Apresentar();
+Calculadora calc = new Calculadora();
+
+
+calc.Somar(10, 3);
+calc.Subtratir(10, 3);
+calc.Multiplicar(10, 3);
+calc.Dividir(10, 3);
+calc.Resto(10, 5);
+calc.Potencia(3, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Pessoa pessoa = new Pessoa();
+
+//pessoa.Nome = "Isaque";
+//pessoa.Idade = 21;
+//pessoa.Apresentar();
