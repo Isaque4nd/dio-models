fab92aed48c5cc2193aaff9af315daf96ecb07e7
Isaque Andrade <andradeisq1.7@gmail.com>
2023-09-29 11:43:31 -0300

    adição dos cálculos trigonométricos e das funções de incremento e decremento

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -38,5 +38,25 @@ namespace diomodels.Models
             Console.WriteLine($"{x}^{y} = {pot}");
         }
 
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {Math.Round(seno, 4)}");
+        }
+
+        public void Conseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo}° = {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo}° = {Math.Round(tangente, 4)}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,16 +4,36 @@
 Calculadora calc = new Calculadora();
 
 
-calc.Somar(10, 3);
-calc.Subtratir(10, 3);
-calc.Multiplicar(10, 3);
-calc.Dividir(10, 3);
-calc.Resto(10, 5);
-calc.Potencia(3, 3);
+//calc.Somar(10, 3);
+//calc.Subtratir(10, 3);
+//calc.Multiplicar(10, 3);
+//calc.Dividir(10, 3);
+//calc.Resto(10, 5);
+//calc.Potencia(3, 3);
 
+//calc.Seno(40);
+//calc.Conseno(30);
+//calc.Tangente(50);
 
+int numeroIncremento = 10;
 
+Console.Write(numeroIncremento);
 
+Console.WriteLine("\n Incrementando o 10");
+
+numeroIncremento++;
+
+Console.WriteLine(numeroIncremento);
+
+int numeroDecremento = 10;
+
+Console.Write(numeroDecremento);
+
+Console.WriteLine("\n Decrementando o 10");
+
+numeroDecremento--;
+
+Console.WriteLine(numeroDecremento);
 
 
 
